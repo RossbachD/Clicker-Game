63c1ef25740a885d8a560b395f495f929245fc3f
Dragir <105174507+RossbachD@users.noreply.github.com>
2025-02-21 10:51:51 +0100

    Added a working timer
    timer added without lose screen

--- my_first_mobile_game/main.gd
+++ my_first_mobile_game/main.gd
@@ -2,9 +2,19 @@ extends Control
 
 
 var score : int = 0
+var time_left : int = 12
 
 func _on_button_pressed() -> void:
 	score +=1
 	$score_label.text = "Score: " + str(score)
 	Input.vibrate_handheld(125)
 	pass # Replace with function body.
+
+
+func _on_time_left_timer_timeout() -> void:
+	time_left -= 1
+	$time_left.text = "Time left: " + str(time_left) +" sec"
+	pass # Replace with function body.
+
+func _ready() -> void:
+	$time_left.text = "Time left: " + str(time_left) + " sec"
---
+++ my_first_mobile_game/menu.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
+	pass # Replace with function body.
